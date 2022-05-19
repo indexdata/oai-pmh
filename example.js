20db2e1494b94b2e1040b166c2dc2cb6c1a87d6e
Mike Taylor <mike@indexdata.com>
2022-05-19 16:28:07 +0100

    Feed ESLint

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import fetch from 'node-fetch';
+// eslint-disable-next-line import/extensions
+import OaiPmh from './index.js';
+
 global.fetch = fetch;
 global.Headers = fetch.Headers;
 global.Request = fetch.Request;
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import OaiPmh from '@indexdata/oai-pmh';
 
 async function listPrefixes(url) {
   const oaiPmh = new OaiPmh(url);
@@ -14,6 +15,7 @@ async function listPrefixes(url) {
 
 listPrefixes('https://ora.ox.ac.uk/oai2').then((formats) => {
   formats.ListMetadataFormats[0].metadataFormat.forEach((x) => {
+    // eslint-disable-next-line no-console
     console.log(`prefix '${x.metadataPrefix} -> ${x.metadataNamespace}`);
   });
 });
